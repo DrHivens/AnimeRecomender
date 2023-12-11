8692c54168bc70f0e0703d02b71867357d091a00
Hivens <salahraquib@gmail.com>
2023-12-11 14:46:44 -0500

    added the recommended anime list source, code needs refactoring

--- ListOfRecommendation.py
+++ ListOfRecommendation.py
@@ -1,11 +1,88 @@
 import json
 import requests
 
-
-
-#  url = "https://api.jikan.moe/v4/recommendations/anime" the only good info in it are the name of the anime and their ids nothing else....
 # get anime ids of all 100 anime
 # make serperate search for these 100 anime 
 # then crossref with the anime
 
+def getAnimeData():
+    url = "https://api.jikan.moe/v4/recommendations/anime"
 
+    # Making a GET request to the Jikan API
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        json_data = response.json()
+
+        extracted_ids = []
+
+        for entry in json_data['data']:
+            for anime_entry in entry['entry']:
+                mal_id = anime_entry['mal_id']
+                extracted_ids.append(mal_id)
+
+        # Writing extracted IDs to a JSON file
+        with open("Extracted_MalIds.json", "w") as json_file:
+            json.dump(extracted_ids, json_file, indent=4)  # Indent for pretty formatting
+
+        return extracted_ids
+    else:
+        print("Failed to fetch data from the Jikan API")
+        return None
+
+# Call the function to get and process data
+#getAnimeData()
+
+
+def getAllAnimeRecommendation(id):
+    url = f"https://api.jikan.moe/v4/anime/{id}"
+    
+    # Making a GET request to the Jikan API for the specific anime ID
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        anime_data = response.json()['data']
+
+        # Extracting specific information
+        title = anime_data['title']
+        studios = [studio['name'] for studio in anime_data['studios']]
+        synopsis = anime_data['synopsis']
+        genres = [genre['name'] for genre in anime_data['genres']]
+
+        # Constructing the extracted information into a dictionary
+        anime_info = {
+            "title": title,
+            "studios": studios,
+            "synopsis": synopsis,
+            "genres": genres
+            # Add more information extraction as needed
+        }
+
+        # Writing extracted data to a JSON file
+        #with open(f"Anime_Info.json", "w") as json_file:
+            #json.dump(anime_info, json_file, indent=4)  # Indent for pretty formatting
+
+        return anime_info
+    else:
+        print("Failed to fetch data from the Jikan API")
+        return None
+
+
+def getStuff():
+    # Read the list of IDs from Extracted_MalIds.json
+    with open("Extracted_MalIds.json", "r") as file:
+        mal_ids = json.load(file)
+
+    # Iterate through each ID and fetch anime information
+    for anime_id in mal_ids:
+        anime_info = getAllAnimeRecommendation(anime_id)
+        
+        if anime_info:
+            # Append the result of the function to anime_info.json
+            with open("anime_info.json", "a") as json_file:
+                json.dump(anime_info, json_file, indent=4)  # Indent for pretty formatting
+                json_file.write("\n")  # Add a new line for separating entries
+
+
+# Call getStuff function
+getStuff()
--- main.py
+++ main.py
@@ -1,42 +1,33 @@
+import json
 import requests
 from collections import Counter
 import spacy
 
-
-#Draw the algorithm
-#Create the algoithm
 #https://www.datacamp.com/tutorial/making-http-requests-in-python
 #https://jikan.docs.apiary.io/#introduction/jikan
 #https://docs.api.jikan.moe/#tag/anime/operation/getAnimeFullById
 #https://github.com/jikan-me/jikan-rest/issues/189
 #https://docs.api.jikan.moe/
+#https://docs.python.org/3/library/collections.html#collections.Counter
 
 
 def getAnimeData(name):
     url = f"https://api.jikan.moe/v4/anime?q={name}]"
 
-    # Making a GET request to the Jikan API
     response = requests.get(url)
 
     if response.status_code == 200:
         json_data = response.json()
 
-        #checks if data is empty in json file
         if 'data' in json_data and len(json_data['data']) == 0:
             print(f"No anime found with the name '{name}'")
             return None
-
-        # if i want to write a json file 
-        #with open("AnimeData.json", "w") as json_file:
-            #json.dump(json_data, json_file, indent=4)  # Indent for pretty formatting
-
+        
         return json_data
     else:
         print("Failed to fetch data from the Jikan API")
         return None
     
-#all info on that anime    
-#anime_data = getAnimeData()
 
 def getAnimeTitle(data):
     return data['data'][0]['title']
@@ -51,56 +42,55 @@ def getAnimeDescription(data):
     return data['data'][0]['synopsis']
 
 
-
-#create the user input
-#Saturday:
-#get the list of anime
-#get data on those anime and stock them in lists
-#Pause and rethink path
-#get list of recommendation and pre parse them
-
-#Sunday:
-#run a function on the reommendation to get their score
-
-
-
-
 animeList = input("Write down your favorite anime seperated with a coma ,  :")
 animeList = animeList.split(',')
-#print(animeList)
-
-animeListGenreRaw = []
-animeListGenre = []
-animeListStudio = []
-animeListDescription = []
-
-for i in animeList:
-    anime = getAnimeData(i)
-    animeListGenreRaw.extend(getAnimeGenre(anime)) # do i want to remove duplicates or nah
-    animeListStudio.append(getAnimeStudio(anime))
-    animeListDescription.append(getAnimeDescription(anime))
-    #print(getAnimeGenre(anime))
-
-genre_counts = Counter(animeListGenreRaw)
-#print(animeListDescription)
-# Filter only the genres that appear more than once
-for genre, count in genre_counts.items():
-    if count > 1:
-        animeListGenre.append(genre)
-
-#print(animeListGenre)
-
-#print(animeListStudio)
-
-#def getAnimeListProperties():
 
 
-nlp = spacy.load("en_core_web_sm")
+def handler(listOfAnimeName):
+    animeListGenreRaw = []
+    animeListGenre = []
+    animeListStudio = []
+    animeListDescriptionRaw = []
+    animeListDescription = []
+
+    filtered_anime_names = [i for i in listOfAnimeName if getAnimeData(i) is not None]
+
+    for i in filtered_anime_names:
+        anime = getAnimeData(i)
+        if anime is not None:
+            animeListGenreRaw.extend(getAnimeGenre(anime))
+            animeListStudio.append(getAnimeStudio(anime))
+            animeListDescriptionRaw.append(getAnimeDescription(anime))
+
+    #genre_counts = Counter(animeListGenreRaw)
+    # Filter only the genres that appear more than once
+    #for genre, count in genre_counts.items():
+        #if count > 1:
+            #animeListGenre.append(genre)
+
+    nlp = spacy.load("en_core_web_sm")
+
+    for description in animeListDescriptionRaw:
+        doc = nlp(description)
+        keywords = [token.text for token in doc if not token.is_stop and not token.is_punct and token.pos_ != 'DET']
+        animeListDescription.extend(keywords)
+
+    # Convert lists to sets to remove duplicates
+    animeListGenre = list(set(animeListGenreRaw))
+    animeListDescription = list(set(animeListDescription))
+
+    # Write attributes to a JSON file
+    data = {
+        "Studio": animeListStudio,
+        "Description": animeListDescription,
+        "Genre": animeListGenre,
+        "Filtered_Anime_Names": filtered_anime_names
+    }
+
+    with open("MyAnimeList.json", 'w') as json_file:
+        json.dump(data, json_file, indent=4)
 
 
-# its meh not the best... will have to think it through ---> needs better filtering
-for description in animeListDescription:
-    doc = nlp(description)
-    keywords = [token.text for token in doc if not token.is_stop and not token.is_punct and token.pos_ != 'DET']
-    keywordsSummary = list(set(keywords))
-    print(keywordsSummary)
+
+
+handler(animeList)
